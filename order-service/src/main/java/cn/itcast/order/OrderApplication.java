8851919a42febf557b4bca53f8de02a8613a3d35
chris <christzhangowner@gmail.com>
2021-12-05 20:38:52 +0800

    nacos config

--- order-service/src/main/java/cn/itcast/order/OrderApplication.java
+++ order-service/src/main/java/cn/itcast/order/OrderApplication.java
@@ -1,5 +1,6 @@
 package cn.itcast.order;
 
+import com.alibaba.cloud.nacos.ribbon.NacosRule;
 import com.netflix.loadbalancer.IRule;
 import com.netflix.loadbalancer.RandomRule;
 import com.netflix.loadbalancer.RoundRobinRule;
@@ -27,8 +28,8 @@ public class OrderApplication {
         return new RestTemplate();
     }
 
-   /* @Bean
-    public IRule randRule(){
-        return new RandomRule();
-    }*/
+    @Bean
+    public IRule nacosRule(){
+        return new NacosRule();
+    }
 }
--- user-service/src/main/java/cn/itcast/user/web/UserController.java
+++ user-service/src/main/java/cn/itcast/user/web/UserController.java
@@ -1,24 +1,38 @@
 package cn.itcast.user.web;
 
+import cn.itcast.user.config.PatternProperties;
 import cn.itcast.user.pojo.User;
 import cn.itcast.user.service.UserService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 @Slf4j
 @RestController
 @RequestMapping("/user")
+//@RefreshScope
 public class UserController {
 
     //region initialize
     private final UserService userService;
 
-    public UserController(UserService userService){
+    // @Value("${pattern.dateformat}")
+    // private String dateformat;
+
+    private final PatternProperties patternProperties;
+
+    public UserController(UserService userService, PatternProperties patternProperties){
         this.userService = userService;
+        this.patternProperties = patternProperties;
     }
     //endregion
 
+    //region queryById
     /**
      * 路径： /user/110
      *
@@ -29,4 +43,19 @@ public class UserController {
     public User queryById(@PathVariable("id") Long id) {
         return userService.queryById(id);
     }
+    //endregion
+
+    //region now
+    @GetMapping("now")
+    public String now(){
+        return LocalDateTime.now().format(DateTimeFormatter.ofPattern(patternProperties.getDateformat()));
+    }
+    //endregion
+
+    //region properties
+    @GetMapping("prop")
+    public  PatternProperties properties(){
+        return  patternProperties;
+    }
+    //endregion
 }
